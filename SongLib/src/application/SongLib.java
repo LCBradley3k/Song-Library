fc308916bcbebd3e39fe87bf7c451e9d251265a0
Mac User <BradleyMitchellis@gmail.com>
2019-02-09 17:16:32 -0500

    Setup blank GridPane

--- SongLib/src/application/SongLib.java
+++ SongLib/src/application/SongLib.java
@@ -3,14 +3,14 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class SongLib extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			GridPane root = new GridPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
