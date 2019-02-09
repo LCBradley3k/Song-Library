f4fb449d1e8ebd8fe38b055b31213756203c2114
Mac User <BradleyMitchellis@gmail.com>
2019-02-09 17:53:25 -0500

    load fxml in application

--- SongLib/src/application/SongLib.java
+++ SongLib/src/application/SongLib.java
@@ -4,23 +4,25 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class SongLib extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			GridPane root = new GridPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../view/SongLib.fxml"));
+		
+		GridPane root = (GridPane)loader.load();
+		Scene scene = new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
