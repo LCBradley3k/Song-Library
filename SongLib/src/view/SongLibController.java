eb73037dab803127ad845cfb22bc349799a4bbc4
Mac User <BradleyMitchellis@gmail.com>
2019-02-11 21:40:02 -0500

    Add songs to separate JSON file, and display their names in listview

--- SongLib/src/view/SongLibController.java
+++ SongLib/src/view/SongLibController.java
@@ -5,6 +5,15 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SongLibController {
 	@FXML
@@ -13,21 +22,49 @@ public class SongLibController {
 	private ObservableList<String> obsList;
 	
 	public void start(Stage mainStage) {
-		// create ObservableList from an ArrayList
-		obsList = FXCollections.observableArrayList(
-			"Thank You, Next",
-			"Sunflower",
-			"Without Me",
-			"Talk",
-			"Going Bad",
-			"Bohemian Rhapsody",
-			"Happier",
-			"Chocolate",
-			"3 Nights",
-			"Harmony Hall"
-		);
-		listView.setItems(obsList);
 		
-		listView.getSelectionModel().select(0);
+		// Open JSON content as string
+		try {
+			String content = readFile("src/data/songs.json");
+			JSONObject song_obj = new JSONObject(content);
+			JSONArray song_info = song_obj.getJSONArray("songs");
+			
+			ArrayList<String> song_names = new ArrayList<String>();
+			
+			for(int i=0; i<song_info.length(); i++) {
+				song_names.add(song_info.getJSONObject(i).getString("name"));
+			}
+			
+			// create ObservableList from an ArrayList
+			obsList = FXCollections.observableArrayList(song_names);
+			listView.setItems(obsList);
+			
+			listView.getSelectionModel().select(0);
+			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	private String readFile(String path) throws IOException {
+		// Open file
+		File file = new File(path);
+		
+		// Build a string from file's contents
+		StringBuilder content = new StringBuilder((int)file.length());
+		
+		try (Scanner scanner = new Scanner(file)) {
+			while(scanner.hasNextLine()) {
+				content.append(scanner.nextLine() + System.lineSeparator());
+			}
+			return content.toString();
+		} 
 	}
 }
