db11d863044c9bf306a08488c87cda60cfa2239d
Mac User <BradleyMitchellis@gmail.com>
2019-02-16 20:17:51 -0500

    connect fxml to methods in controller

--- SongLib/src/view/SongLibController.java
+++ SongLib/src/view/SongLibController.java
@@ -3,7 +3,10 @@ package view;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,8 +19,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SongLibController {
-	@FXML
-	ListView<String> listView;
+	@FXML ListView<String> listView;
+	@FXML Button edit;
+	@FXML Button add;
+	@FXML Button delete;
+	@FXML Text song;
+	@FXML Text artist;
+	@FXML Text album;
+	@FXML Text year;
+	
 	
 	private ObservableList<String> obsList;
 	
@@ -66,5 +76,20 @@ public class SongLibController {
 			}
 			return content.toString();
 		} 
+	}
+	
+	public void editAction(ActionEvent e) {
+		System.out.println("Edit Button Working");
+		/* To Do */
+	}
+	
+	public void addAction(ActionEvent e) {
+		System.out.println("Add Button Working");
+		/* To Do */
+	}
+	
+	public void deleteAction(ActionEvent e) {
+		System.out.println("Delete Button Working");
+		/* To Do */
 	}
 }
